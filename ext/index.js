4632fd110afffede9ec06ca33db9fce4654dd905
dungdanghung <dungprof8.0@gmail.com>
2023-05-25 17:48:22 +0700

    update

--- ext/index.js
+++ ext/index.js
@@ -18,7 +18,10 @@ let check = true
 let timeout;
 const icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 21 21"><defs><style>.cls-1 {fill: none;}.cls-2 {fill: #1d1d1d;}</style></defs><g id="Group_1" data-name="Group 1" transform="translate(-397 -369)"><rect id="Rectangle_2" data-name="Rectangle 2" class="cls-1" width="21" height="21" transform="translate(397 369)"/><g id="download_copy" data-name="download copy" transform="translate(397 369.2)"><path id="Path_5" data-name="Path 5" class="cls-2" d="M20.3,13.8a1.136,1.136,0,0,0-.8-.3h0a1.11,1.11,0,0,0-1.1,1.1v3.8H2.2V14.6a1.11,1.11,0,0,0-1.1-1.1A1.11,1.11,0,0,0,0,14.6v4.9a1.135,1.135,0,0,0,.3.8,1.135,1.135,0,0,0,.8.3H19.6a1.11,1.11,0,0,0,1.1-1.1V14.6A1.506,1.506,0,0,0,20.3,13.8Z"/><path id="Path_6" data-name="Path 6" class="cls-2" d="M9.8,15l.2.2a1.09,1.09,0,0,0,1.1-.2L16,9.8a1.061,1.061,0,0,0-1.5-1.5l-3,3.2V1.1A1.11,1.11,0,0,0,10.4,0,1.135,1.135,0,0,0,9.2,1.1V11.4L6.3,8.2a2.01,2.01,0,0,0-.8-.4,1.08,1.08,0,0,0-.7.3,1.116,1.116,0,0,0-.1,1.6Z"/></g></g></svg>'
 const ITEM_TEMPLATE = `<div id="downloadimage">${icon}</div>`
-const itemDOM = new DOMParser().parseFromString(ITEM_TEMPLATE, 'text/html').body.firstElementChild
+const itemDOM1 = new DOMParser().parseFromString(ITEM_TEMPLATE, 'text/html').body.firstElementChild
+const POSTPAGEICON = itemDOM1.addEventListener("click", getdata("post"))
+const itemDOM2 = new DOMParser().parseFromString(ITEM_TEMPLATE, 'text/html').body.firstElementChild
+const HOMEPAGEICON = itemDOM2.addEventListener("click", getdata("home"))
 
 
 const body = document.querySelector("body")
@@ -27,19 +30,17 @@ const callback = (mutationList, observe) => {
         clearTimeout(timeout)
         const b = document.querySelector("._aatk")
         const c = document.querySelector(".x1iyjqo2.x1tjbqro.x78zum5.x6s0dn4.xeud2gj")
-        if (b && !document.querySelector(".downloadimage")) {
+        if (b && !document.querySelector("#downloadimage")) {
             check = false
-            itemDOM.addEventListener("click", getdata("home"))
-            b.appendChild(itemDOM)
+            b.appendChild(itemDOM2)
             timeout = setTimeout(() => {
                 check = true
             }, 1000)
         }
-        else if (c && !document.querySelector(".downloadimage")) {
+        else if (c && !document.querySelector("#downloadimage")) {
             check = false
             c.style.position = "relative"
-            itemDOM.addEventListener("click", getdata("post"))
-            c.appendChild(itemDOM)
+            c.appendChild(itemDOM1)
             timeout = setTimeout(() => {
                 check = true
             }, 1000)
@@ -114,7 +115,3 @@ function getdata(type) {
         }
     }
 }
-
-
-// document.querySelectorAll(".x1iyjqo2.x1tjbqro.x78zum5.x6s0dn4.xeud2gj")
-// document.querySelectorAll("._aamj._acvz._acnc._acng")
